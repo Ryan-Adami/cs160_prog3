25bcc11b771bd6c866ac2ffc66da5f605f6ca195
Ethan Daniel <ethandaniel@berkeley.edu>
2022-03-10 21:24:43 -0800

    screen 1 done

--- app/src/main/java/com/example/cs160_prog3/MainActivity.java
+++ app/src/main/java/com/example/cs160_prog3/MainActivity.java
@@ -2,17 +2,18 @@ package com.example.cs160_prog3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-import com.example.cs160_prog3.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == 0)
+                    setContentView(R.layout.activity_main);
+                else
+                    setContentView(R.layout.activity_profile);
+                return true;
+            }
+        });
     }
 
     public void sendMessage(View view) {
@@ -33,4 +46,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
     }
+
+
 }
