45b0d7b79bccf8dedfedb4f6bcf968d3b5d7b753
Ethan Daniel <ethandaniel@berkeley.edu>
2022-03-10 22:24:58 -0800

    fix nav bar icons, link screens 1<-->2-->3

--- app/src/main/java/com/example/cs160_prog3/Profile.java
+++ app/src/main/java/com/example/cs160_prog3/Profile.java
@@ -1,20 +1,35 @@
 package com.example.cs160_prog3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.cs160_prog3.databinding.ActivityProfileBinding;
 import com.example.cs160_prog3.databinding.ActivityRunTrackerBinding;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Profile extends AppCompatActivity {
     private ActivityProfileBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == 0)
+                    setContentView(R.layout.activity_main);
+                else
+                    setContentView(R.layout.activity_profile);
+                return true;
+            }
+        });
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
--- app/src/main/java/com/example/cs160_prog3/SelectWorkout.java
+++ app/src/main/java/com/example/cs160_prog3/SelectWorkout.java
@@ -1,8 +1,13 @@
 package com.example.cs160_prog3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cs160_prog3.databinding.ActivityProfileBinding;
@@ -13,5 +18,29 @@ public class SelectWorkout extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choices);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    public void startRunningActivity(View view) {
+        Intent intent = new Intent(this, RunTracker.class);
+        startActivity(intent);
     }
 }
