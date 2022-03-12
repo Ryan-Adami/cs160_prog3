49788b1162f76e76c936a6c739927534cf37954f
Ethan Daniel <ethandaniel@berkeley.edu>
2022-03-11 18:47:46 -0800

    revert changes on screen 4

--- app/src/main/java/com/example/cs160_prog3/RunTracker.java
+++ app/src/main/java/com/example/cs160_prog3/RunTracker.java
@@ -1,13 +1,9 @@
 package com.example.cs160_prog3;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 
